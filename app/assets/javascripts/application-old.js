b2ee8a15f9b203d778d101cd22f64bf0d37717ae
greenlakes <greenlakes2005@gmail.com>
2017-02-27 18:55:12 +0000

    working contact form

--- app/assets/javascripts/application-old.js
+++ app/assets/javascripts/application-old.js
@@ -12,5 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+/* ========================================================================
+ * Bootstrap: carousel.js v3.3.7
+ * http://getbootstrap.com/javascript/#carousel
+ * ========================================================================
+ * Copyright 2011-2016 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+
+
